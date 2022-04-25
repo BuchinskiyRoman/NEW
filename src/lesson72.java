79168ecb54d4feca33a732de675ec2d00c0b4351
BuchinskiyRoman <buchinskiyroman@gmail.com>
2022-04-25 22:02:08 +0300

    finish

--- src/lesson72.java
+++ src/lesson72.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class lesson72 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //Напишите программу на Java для создания новой строки, берущей первые три символа
+                        // из заданной строки. Если длина данной строки меньше 3, используйте «#» в качестве замещающих символов.
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Type sentences: ");
@@ -9,11 +10,12 @@ public class lesson72 {
 
         int lineNum = line.length();
          if (lineNum>=3){
-             System.out.println(line.substring(0,2));
-
-
-
-
-        }
+             System.out.println(line.substring(0,3));
+             System.out.println(line.length());
+         }else if (lineNum==1){
+             System.out.println(line.charAt(0)+"#"+"#");
+         } else{
+             System.out.println("#"+"#"+"#");
+         }
     }
 }
