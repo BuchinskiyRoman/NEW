c5d37d1ab880bd6da4eb24498d3063fa7a869746
BuchinskiyRoman <buchinskiyroman@gmail.com>
2022-04-29 13:47:35 +0300

    finish

--- src/less101.java
+++ src/less101.java
@@ -2,9 +2,19 @@ public class less101 {
     public static void main(String[] args) { // Write a Java program to check if the number of 10 is greater than number
                                             // to 20s in a given array of integers.
 
-        int[] num = {10,20,56,10,10,20,0,2,6,2,1,20};
-        int res = 1;
+        int[] num = {10,20,56,10,10,20,20,2,6,2,1,20};
+        int ten = 0;
+        int too = 0;
 
-
+        for (int i = 0; i <=num.length-1; i++){
+            if (num[i]==10){
+                ten++;
+            }
+            if (num[i]==20){
+                too++;
+            }
+        }
+        System.out.println("ten: "+ten);
+        System.out.println("too: "+too);
     }
 }
